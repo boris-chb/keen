41c9b7d7154e642b24e403cf110bd7bc6299cf13
Boris Ciobirca <X00167171@myTUDublin.ie>
2020-12-01 20:13:32 +0200

    Added navbar

--- keen/settings.py
+++ keen/settings.py
@@ -119,13 +119,25 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+CRISPY_TEMPLATE_PACK = 'uni_form'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.name
+    
+def upload_path(instance, filename):
+    return f'{instance.product.category}/{instance.product.name}/{filename}'
+
+class Product(models.Model):
+    """
+        * Using db_index model attribute to improve query performance.
+          (by default, only PK are indexed)
+    """
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='upload_path', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('name', 'created', 'available',)
+        # Index on ID+Slug fields for better query performance on both fields
+        index_together = (('id', 'slug'),)
+    
+    def __str__(self):
+        return self.name
+    
